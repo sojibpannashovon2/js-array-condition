65edcd5bd793aeb67248c4192e463f5a270afe02
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-02 23:27:50 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+var number = [10, 20, 30, 40, 50, 60, 100];
+
+console.log(number);
+
+var char = ['rahim', 'karim', 'jobbar', 'mosarof'];
+
+console.log(char);
+
+console.log(char.length);
+
+
+console.log(number.length);
+
