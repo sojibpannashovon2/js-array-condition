6309927dbda75e760786137928d7b2dc2d3ef85b
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-03 00:00:10 +0600

    pop-push commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var p = [73, 56, 90, 99, 40, 1000];
+
+console.log(p);
+
+// get the value from a array index
+
+console.log(p[1]);
+
+console.log(p.length);
+console.log(p[5]);
+
+console.log(p[3]);
+
+// set the element value by index
+
+p[3] = 5000;
+
+console.log(p);
+
+p[8] = 90;
+
+console.log(p);
+
+// find the index of an element
+
+var positionIndex = p.indexOf(1000);
+
+console.log(positionIndex);
+
+var positionIndex = p.indexOf(73);
+
+console.log(positionIndex);
+
+var positionIndex = p.indexOf(56);
+
+console.log(positionIndex);
+
+
---
+++ push-pop.js
@@ -0,0 +1,28 @@
+
+
+var num = [3, 5, 9, 5, 2, 7, 8, 10];
+
+console.log(num);
+
+
+var string = ['gelam', 'pelam', 'tumi', 'ami', 'ratna'];
+
+console.log(string);
+
+// Adding item in last position
+
+num.push(77);
+
+console.log(num);
+
+t = num.push(58098048);
+
+console.log(num);
+
+
+// Removing item in last position
+
+num.pop();
+
+console.log(num);
+
