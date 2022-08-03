0b68535f6d2f11d483119d2a8397c115b67ab00f
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-03 20:45:06 +0600

    last commit

---
+++ practice.js
@@ -0,0 +1,42 @@
+
+
+// var myself = 85;
+
+// var tom = 66;
+
+// var jane = 95;
+
+// var peter = 66;
+
+// var jhon = 40;
+
+var marks = 86;
+
+if (marks >= 80 && marks <= 100) {
+    console.log("A Grade");
+}
+else if (marks >= 70 && marks <= 79) {
+    console.log("B Grade");
+}
+else if (marks >= 60 && marks <= 69) {
+    console.log("C Grade");
+}
+else if (marks >= 50 && marks <= 59) {
+    console.log("D Grade");
+}
+else {
+    console.log("Fail");
+}
+
+var p = 9;
+
+var q = 8;
+
+var r = 9;
+
+if (p == q || p == r || q == r) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
